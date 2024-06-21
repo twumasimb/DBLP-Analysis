d0aa40ab886035cf3421ddbc6abf0cba6a342408
Twumasi Mensah-Boateng <twumasimb@gmail.com>
2024-06-20 22:09:16 -0500

    update influence algo

--- algos.py
+++ algos.py
@@ -60,15 +60,65 @@ def leader_eff(graph_G, graph_P, metric_fn, node, beta=None):
 
 
 def intra_team_rank(graph_G, metric_fn, node) -> int:
-    team_graph = ps.subgraph_by_same_label(graph_G, node)  # Subgraph of nodes with the same label
+    # Subgraph of nodes with the same label
+    team_graph = ps.subgraph_by_same_label(graph_G, node)
     centrality = metric_fn(team_graph)  # Centrality measure on the team graph
     ranked_nodes = sorted(centrality, key=centrality.get, reverse=True)
     node_rank = ranked_nodes.index(node) + 1
     return int(node_rank)
 
+
 def inter_team_rank(graph_G, graph_P, metric_fn, node) -> int:
-    team_graph = ps.subgraph_by_label(graph_G, graph_P, node)  # Subgraph of nodes with the same label
+    # Subgraph of nodes with the same label
+    team_graph = ps.subgraph_by_label(graph_G, graph_P, node)
     centrality = metric_fn(team_graph)  # Centrality measure on the team graph
     ranked_nodes = sorted(centrality, key=centrality.get, reverse=True)
     node_rank = ranked_nodes.index(node) + 1
     return int(node_rank)
+
+
+def randomMonteCarlo(graph_G, graph_P, metric_fn, num_iter):
+    """
+        We calculate the weight of the adjacent nodes the selected node is connected to in its team.
+        We then calculate the weight of the subgraph formed by the selected nodes.(iterteam communication)
+    """
+    total_weight = 0
+
+    for _ in range(num_iter):
+
+        total_weight += sum(leader_eff(graph_G, graph_P, metric_fn,
+                            node, beta=None) for node in ps.randomAlgo(graph_G))
+
+    return round(total_weight / num_iter, 2)
+
+
+def get_top_node_from_each_group(graph_G, graph_P, metric_fn):
+    top_nodes = []
+    for label in graph_P.nodes:
+        team_members = [n for n, attr in graph_G.nodes(
+            data=True) if attr['label'] == label]
+        team_graph = graph_G.subgraph(team_members)
+        centrality = metric_fn(team_graph)
+        ranked_nodes = sorted(centrality, key=centrality.get, reverse=True)
+        top_node = ranked_nodes[0] if ranked_nodes else None
+        top_nodes.append(top_node)
+    return top_nodes
+
+
+def inteam_influence_only(graph_G, graph_P, metric_fn):
+    """
+    Calculate the influence of the seed node on its team members only
+    """
+    network = graph_G.subgraph(get_top_node_from_each_group(graph_G, graph_P, metric_fn))
+
+    print("Intra-team ranking")
+    for node in network.nodes():
+        print(f"Team :{graph_G.copy().nodes[node]['label']}, Node: {node}, Rank: {intra_team_rank(graph_G, metric_fn, node)}")
+
+    print("\n")
+
+    print("Inter-team ranking")
+    for node in network.nodes():
+        print(f"Team :{graph_G.copy().nodes[node]['label']}, Node: {node}, Rank: {inter_team_rank(graph_G, graph_P, metric_fn, node)}")
+    
+    return sum(leader_eff(graph_G, graph_P, metric_fn, user, beta=None) for user in network)
